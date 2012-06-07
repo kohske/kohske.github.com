b6d67fdf772d6e68199a806a02fffa72801df6b5
Kohske Takahashi @ jurina <takahashi.kohske@gmail.com>
2012-06-08 02:23:21 +0900

    Update why-slidify with the latest version.

--- sandbox/slidify/why-slidify/generator.R
+++ sandbox/slidify/why-slidify/generator.R
@@ -2,7 +2,10 @@ library(knitr)
 library(slidify)
 
 # generate static html
+options(markdown.HTML.options=c(markdownHTMLOptions(default=TRUE), "highlight_code"))
 knit2html("whyslidify.Rmd", output = "whyslidify_static.html")
 
 # generate HTML5 slide
-slidify("whyslidify.Rmd")
+opts <- slidifyOptions()
+opts$copy_libraries <- TRUE
+slidify("whyslidify.Rmd", options = opts)
--- sandbox/slidify/why-slidify/whyslidify.md
+++ sandbox/slidify/why-slidify/whyslidify.md
@@ -1,5 +1,6 @@
 
 
+---
 
 # Markdown, Knitr, and Slidify
 
